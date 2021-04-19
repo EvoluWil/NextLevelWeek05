0db8ec25db4e5e66ad4a60e09eba8ead533e8d02
Willian Git <57255222+WrsDeveloper@users.noreply.github.com>
2021-04-19 11:44:26 -0300

    Dia 1
    primeiro dia da NLW05

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { request, response } from "express";
+
+
+const app = express();
+
+app.get("/", (request, require)=>{
+
+})
+
+app.post("/", (request, response)=>{
+    
+})
+
+
+app.listen(3333, ()=>console.log("server is running on port 3333"));
